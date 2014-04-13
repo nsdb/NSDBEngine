bf22fb3627b49f897070d6c217c38c528c3786c4
NSDB <nsdb@naver.com>
2014-04-13 16:08:41 +0900

    LinedCircle bug fix

--- NSDBEngine/src/com/nsdb/engine/core/opengl/LinedCircle.java
+++ NSDBEngine/src/com/nsdb/engine/core/opengl/LinedCircle.java
@@ -6,9 +6,10 @@ public class LinedCircle extends Circle {
 	public LinedCircle(float radius,int polyCount,float lineWidth) {
 		super(radius,polyCount);
 		// line buffer
-		byte[] indices=new byte[polyCount];
+		byte[] indices=new byte[polyCount+1];
 		for(int i=0;i<polyCount;i++)
 			indices[i]=(byte)(i+1);
+		indices[polyCount]=indices[0];
 		setLine(indices, lineWidth);
 	}
 }
